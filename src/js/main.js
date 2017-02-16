e23bedbd1948b6c42a738ed181d89ed71026297e
Endorel <helene.francke@gmail.com>
2017-02-16 14:57:59 +0100

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,36 @@
 "use strict"
 
-//jQuery
 
-$(document).ready(function() {
 
-	console.log('Sidan har laddats!');
+var mq_device = window.matchMedia( "(max-width: 1024px)" );
+var mq_desktop = window.matchMedia( "(min-width: 1025px)" );
 
-	$("#sub").click(function() {
-		//e.preventDefault();
-		$(".submenu").slideToggle('slow');
-		console.log('Länken klickas')
+if (mq_device.matches) {
+	$(".roseText").click(function() {
+		$( this).find('.slideInfo').toggle();
+		//$('.roseTitle', this).css({'color' : 'black'});
+
 	});
+} else if (mq_desktop.matches) {
 	$(".roseText").mouseenter(function() {
-		$( this).find('.slideInfo').slideToggle('slow');
-		//$(".slideInfo", this).css({'display' : 'inline-block'});
+		$(this).find('.slideInfo').toggle();
+		$(".slideInfo", this).css({'display' : 'block'});
 		$('.roseTitle', this).css({'color' : 'black'});
 		//alert('Hover registreras');
 	});
 	$('.roseText').mouseleave(function() {
-		$(this).find('.slideInfo').slideToggle('slow');
+		$(this).find('.slideInfo').toggle();
 		$('.roseTitle', this).css({'color' : '#fff'});
 	});
-	
-});
+}
 
+console.log('Sidan har laddats!');
+
+$("#sub").click(function () {
+	//e.preventDefault();
+	$(".submenu").slideToggle('slow');
+	console.log('Länken klickas')
+});
 
 
 //Functions
